c3cb517de8aed159ff1932bd8a765276a02feb4e
Filipe Varjão <varjaofilipe@gmail.com>
2019-10-08 10:32:55 -0300

    first commit

---
+++ lib/ex_watson_tone.ex
@@ -0,0 +1,18 @@
+defmodule ExWatsonTone do
+  @moduledoc """
+  Documentation for ExWatsonTone.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExWatsonTone.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_watson_tone_test.exs
@@ -0,0 +1,8 @@
+defmodule ExWatsonToneTest do
+  use ExUnit.Case
+  doctest ExWatsonTone
+
+  test "greets the world" do
+    assert ExWatsonTone.hello() == :world
+  end
+end
